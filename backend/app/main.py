3b52c86fdd4a2a75eddff6809e297b1ff9e21b74
richa <riclarobr04@gmail.com>
2025-11-26 17:13:24 -0500

    test

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,11 +16,12 @@ def get_application() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.allowed_origins,
+        allow_origins=["*"],
         allow_credentials=True,
-        allow_methods=["GET", "POST", "PUT", "DELETE"],
-        allow_headers=["Content-Type", "Authorization"],
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
+
 
     app.include_router(auth.router, prefix="/api")
     app.include_router(products.router, prefix="/api")
